c51ad64be6cf7583753df836b895169afb2e0a53
algos-soft <info@algos.it>
2022-12-24 15:53:18 +0100

    ...

---
+++ src/test/java/it/algos/utility/DialogTest.java
@@ -0,0 +1,77 @@
+package it.algos.utility;
+
+import it.algos.*;
+import it.algos.base.*;
+import it.algos.vaad24.ui.dialog.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.test.context.junit.jupiter.*;
+
+/**
+ * Project vaad24
+ * Created by Algos
+ * User: gac
+ * Date: Thu, 22-Dec-2022
+ * Time: 08:39
+ * Estende la classe astratta AlgosTest che contiene costanti e metodi base <br>
+ * Nella superclasse AlgosTest vengono iniettate (@InjectMocks) tutte le altre classi di service <br>
+ * Nella superclasse AlgosTest vengono regolati tutti i link incrociati tra le varie classi singleton di service <br>
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {Vaad24SimpleApplication.class})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Tag("quickly")
+@Tag("enums")
+@DisplayName("Enumeration AEDialog")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DialogTest extends AlgosIntegrationTest {
+
+
+    /**
+     * Qui passa una volta sola, chiamato dalle sottoclassi <br>
+     * Invocare PRIMA il metodo setUpStartUp() della superclasse <br>
+     * Si possono aggiungere regolazioni specifiche <br>
+     */
+    @BeforeAll
+    protected void setUpAll() {
+        super.setUpAll();
+    }
+
+
+    /**
+     * Qui passa prima di ogni test delle sottoclassi <br>
+     * Invocare PRIMA il metodo setUp() della superclasse <br>
+     * Si possono aggiungere regolazioni specifiche <br>
+     */
+    @BeforeEach
+    protected void setUpEach() {
+        super.setUpEach();
+    }
+
+
+    @Test
+    @Order(1)
+    @DisplayName("1 - avviso")
+    void avviso() {
+        sorgente = "Prima prova di avviso";
+        Avviso.show(sorgente);
+    }
+
+
+    /**
+     * Qui passa al termine di ogni singolo test <br>
+     */
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    /**
+     * Qui passa una volta sola, chiamato alla fine di tutti i tests <br>
+     */
+    @AfterAll
+    void tearDownAll() {
+    }
+
+}
