ba73b575dde8f077520487c7e4821d6a164e8238
algos-soft <info@algos.it>
2023-04-17 17:45:04 +0200

    ....

--- src/main/java/it/algos/vaad24/backend/service/AnnotationService.java
+++ src/main/java/it/algos/vaad24/backend/service/AnnotationService.java
@@ -47,6 +47,7 @@ public class AnnotationService extends AbstractService {
 
     public AnnotationService() {
     }
+
     //==========================================================================
     // Costanti
     //==========================================================================
