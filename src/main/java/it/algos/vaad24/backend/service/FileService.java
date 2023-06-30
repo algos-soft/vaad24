6fbdd9725947fdcc673378bd8f0bfc91c32c7c18
algos-soft <info@algos.it>
2023-06-30 17:11:35 +0200

    ...

--- src/main/java/it/algos/vaad24/backend/service/FileService.java
+++ src/main/java/it/algos/vaad24/backend/service/FileService.java
@@ -654,7 +654,7 @@ public class FileService extends AbstractService {
      * @return wrapper di informazioni risultanti
      */
     public AResult copyFile(final AECopy typeCopy, String srcPathDir, String destPathDir, final String nomeFile, String srcToken, String destToken) {
-        AResult result = AResult.build().method("copyFile").target(nomeFile);
+        AResult result = AResult.build().valido(true).method("copyFile").target(nomeFile);
         String message;
         String srcPath;
         String destPath;
@@ -2163,7 +2163,7 @@ public class FileService extends AbstractService {
     }
 
     public AResult checkFileDirectory(final String methodName, final File fileDirectoryToBeChecked) {
-        AResult result = AResult.build().method(methodName).valido(true).target(fileDirectoryToBeChecked.getAbsolutePath());
+        AResult result = AResult.build().valido(true).method(methodName).target(fileDirectoryToBeChecked.getAbsolutePath());
         String message;
 
         if (fileDirectoryToBeChecked == null) {
