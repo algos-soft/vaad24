c080b0b034439d06c62c78f2de0f6521862fe2cc
algos-soft <info@algos.it>
2023-09-21 21:19:31 +0200

    flag

--- src/main/java/it/algos/vaad24/backend/utility/UtilityView.java
+++ src/main/java/it/algos/vaad24/backend/utility/UtilityView.java
@@ -118,17 +118,30 @@ public class UtilityView extends VerticalLayout {
     }
 
     public void body() {
-        this.paragrafoFlag();
+        this.paragrafoCriticalFlag();
+        this.paragrafoAllFlag();
         this.paragrafoPreferenze();
         this.paragrafoReset();
     }
 
-    public void paragrafoFlag() {
+    public void paragrafoCriticalFlag() {
         VerticalLayout layout = new VerticalLayout();
         layout.setMargin(false);
         layout.setPadding(false);
         layout.setSpacing(false);
-        H3 paragrafo = new H3("Flag critical");
+        H3 paragrafo = new H3("Critical flag");
+        paragrafo.getElement().getStyle().set("color", "blue");
+
+        this.add(paragrafo);
+        this.add(layout);
+    }
+
+    public void paragrafoAllFlag() {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(false);
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        H3 paragrafo = new H3("All flag");
         paragrafo.getElement().getStyle().set("color", "blue");
 
         this.add(paragrafo);
